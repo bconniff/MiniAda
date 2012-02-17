486bcb75de3a9664f5908c2014945c1b8f112d46
Brendan Conniff <niffshack@gmail.com>
2012-02-17 13:37:02 -0500

    added case insensitve FileStream for parser

---
+++ MiniAdaFileStream.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CharStream;
+
+public class MiniAdaFileStream extends ANTLRFileStream {
+   public MiniAdaFileStream(String s) throws IOException {
+      super(s, null);
+   }
+
+   public MiniAdaFileStream(String s, String enc) throws IOException {
+      super(s, enc);
+   }
+
+   public int LA(int i) {
+      if (i == 0) return 0;
+      if (i < 0) i++;
+
+      return ((p + i - 1) >= n)
+         ? CharStream.EOF
+         : Character.toLowerCase(data[p+i-1]);
+   }
+}
