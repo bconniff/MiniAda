3796abcc4fe437d6f4c6948f8d53a1caac2c3e4a
Joel Dominic <jdominic@rucs.radford.edu>
2012-03-14 09:36:33 -0400

    Added comments to the AbstractTreeNode class

--- trees/print/AbstractTreeNode.java
+++ trees/print/AbstractTreeNode.java
@@ -7,6 +7,7 @@ public abstract class AbstractTreeNode {
 
 	private static final HashSet<Class> BASIC;
 
+	//Builds a set of the primitive types
 	static {
 		BASIC = new HashSet<Class>();
 		BASIC.add(Integer.class);
@@ -18,19 +19,27 @@ public abstract class AbstractTreeNode {
 		BASIC.add(Character.class);
 	}
 
+	//Returns a JSON style string of the current object
 	public String toString() {
+		//Get all the fields in the object
 		Field[] fields = getClass().getDeclaredFields();
+		//Create a buffer to build the string
 		StringBuffer toRet = new StringBuffer();
 		toRet.append("{");
 		for(Field field : fields) {
 			try {
+				//If the field is private allow it to be accessed
 				field.setAccessible(true);
 				Object obj = field.get(this);
+				//Add name and colon
 				toRet.append(field.getName());
 				toRet.append(" : ");
 				if(obj != null)
+					//If the object is an array
 					if(obj.getClass().isArray()) {
+						//Get the number of dimensions
 						int dims = 1 + obj.getClass().getName().lastIndexOf('[');
+						//Loop through and add all the values
 						for(int n = 0;n < dims;n++) {
 							toRet.append("[");
 							int length = Array.getLength(obj);
@@ -38,24 +47,30 @@ public abstract class AbstractTreeNode {
 								toRet.append(Array.get(obj, i).toString());
 								toRet.append(", ");
 							}
+							//Remove the last space and comma
 							toRet.delete(toRet.length()-2, toRet.length());
 							toRet.append("]");
 						}
 					} else {
+						//If a primitive, just add it to the buffer
 						if(BASIC.contains(obj.getClass())) {
 							toRet.append(obj.toString());
+						//If a string, add quotes around
 						} else if(obj.getClass() == String.class) {
 							toRet.append("\"");
 							toRet.append(obj.toString());
 							toRet.append("\"");
+						//If a collection of objects don't add brackets
 						} else if(obj instanceof Collection) {
 							toRet.append(obj.toString());
+						//Default object behavior, print out the object with brackets around it
 						} else {
 							toRet.append("{");
 							toRet.append(obj.toString());
 							toRet.append("}");
 						}
 					}
+				//null object
 				else {
 					toRet.append("null");
 				}
@@ -64,6 +79,7 @@ public abstract class AbstractTreeNode {
 				e.printStackTrace();				
 			}
 		}
+		//Remove trailing space and comma again
 		toRet.delete(toRet.length()-2, toRet.length());
 		toRet.append("}");
 		return toRet.toString();
